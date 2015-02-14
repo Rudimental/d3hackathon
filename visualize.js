8b442fa84f1e9b982c830b921be44a6dd7e45e3a
ThornWinters <MichaelJWarner@hotmail.com>
2015-02-13 22:08:16 -0800

    linked the data to the graph

--- visualize.js
+++ visualize.js
@@ -4,23 +4,80 @@
 // }
 
 
-var tempData = chicago.response.songs;
+var chicagoData = chicago.response.artists;
 
-var container = d3.select(".container");
+var chicago = d3.select(".chicago");
 
-container
+chicago
   .selectAll("div")
-    .data(tempData)
+    .data(chicagoData)
   .enter().append("div")
     .style({
-      "width" : function(d) { return d.song_hotttnesss * 100 + "%";},
+      "width" : function(d) { return d.hotttnesss * 100 + "%";},
       "margin" : "10px",
       "height" : "25px",
       "font-family" : "Helvetica Nueue,Helvetica,Sans-Sarif",
       "background-color" : "green",
       "padding-top" : "10px"
       })
-    .text(function(d) { return d.artist_name; });
+    .text(function(d) { return d.name; });
 
 
+
+var austinData = austin.response.artists;
+
+var austin = d3.select(".austin");
+
+austin
+  .selectAll("div")
+    .data(austinData)
+  .append("div")
+    .style({
+      "width" : function(d) { return d.hotttnesss * 100 + "%";},
+      "margin" : "10px",
+      "height" : "25px",
+      "font-family" : "Helvetica Nueue,Helvetica,Sans-Sarif",
+      "background-color" : "blue",
+      "padding-top" : "10px"
+      })
+    .text(function(d) { return d.name; });
+
+
+//---------------
+var san_franciscoData = sanfrancisco.response.artists;
+
+var san_francisco = d3.select(".san_francisco");
+
+san_francisco
+  .selectAll("div")
+    .data(san_franciscoData)
+  .append("div")
+    .style({
+      "width" : function(d) { return d.hotttnesss * 100 + "%";},
+      "margin" : "10px",
+      "height" : "25px",
+      "font-family" : "Helvetica Nueue,Helvetica,Sans-Sarif",
+      "background-color" : "blue",
+      "padding-top" : "10px"
+      })
+    .text(function(d) { return d.name; });
+
+//---------------
+var new_yorkData = newyork.response.artists;
+
+var new_york = d3.select(".new_york");
+
+new_york
+  .selectAll("div")
+    .data(new_yorkData)
+  .append("div")
+    .style({
+      "width" : function(d) { return d.hotttnesss * 100 + "%";},
+      "margin" : "10px",
+      "height" : "25px",
+      "font-family" : "Helvetica Nueue,Helvetica,Sans-Sarif",
+      "background-color" : "blue",
+      "padding-top" : "10px"
+      })
+    .text(function(d) { return d.name; });
 
